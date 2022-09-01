35b011611ca2b34d6b4d0242bc395f4ce27fdaae
Mary-Wanyama <mary.wanyama@student.moringaschool.com>
2022-09-01 04:20:32 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault();
+  // get data from the submitted form
+  const myForm = document.getElementById("create-task-form");
+  myForm.addEventListener("submit", (e) => {
+    // Prevent the default Value
+    e.preventDefault();
+    const newTask = document.getElementById("new-task-description");
+    const taskList = document.getElementById("tasks");
+    const newTaskItem = document.createElement("li");
+    newTaskItem.innerText = newTask.value;
+    taskList.appendChild(newTaskItem);
+    e.target.reset();
+  });
 });
